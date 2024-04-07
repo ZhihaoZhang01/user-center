f0a91c7e8ab505c8234b09809f1b5b2d8f707cd1
wirepuller <374154380@qq.com>
2024-04-06 21:23:53 -0400

    First commit

---
+++ user-center-backend/src/main/java/com/zhihao/usercenterbackend/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.zhihao.usercenterbackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhihao.usercenterbackend.model.User;
+
+/**
+ * @author Zhihao Zhang
+ * @description TODO
+ * @date 2024-04-06 6:09 PM
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
